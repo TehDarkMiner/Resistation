ef95a1b7dccd7bc05ce209527f390a895a2858d9
TehDarkMiner <andrewschom@live.com>
2013-08-15 19:01:14 +0930

    Add player, basic movement begun, update.
    Added temporary left movement only. Added event handling, drawing,
    player, and an update method.

--- engine.py
+++ engine.py
@@ -1,6 +1,25 @@
-
+import pyglet
+from pyglet.window import key
 
 class World:
     def __init__(self,width,height):
         self.width = width
         self.height = height
+
+class Player:
+    def __init__(self,x,y,image):
+        self.x = x
+        self.y = y
+        self.sprite = pyglet.sprite.Sprite(image,x=x,y=y)
+        self.key_handler = key.KeyStateHandler()
+
+    def update(self):
+        self.sprite.x = self.x
+        self.sprite.y = self.y
+        if self.key_handler[key.A]:
+            self.x -= 1
+            print 'a'
+
+
+    def draw(self):
+        self.sprite.draw()
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import engine
 import pyglet
 
+playerImage = pyglet.image.load('resources/player.png')
+
 window = pyglet.window.Window(640,480)
 world = engine.World(640,480)
+player = engine.Player(10,10,playerImage)
+
+window.push_handlers(player.key_handler)
+
+@window.event
+def on_draw():
+    player.draw()
+
+def update(dt):
+    player.update()
+
+pyglet.clock.schedule_interval(update,1.0/120.0)
 
 pyglet.app.run()
