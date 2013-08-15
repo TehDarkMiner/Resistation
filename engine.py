746cc892572fcaf3392ced331601c9324929ebd6
TehDarkMiner <andrewschom@live.com>
2013-08-15 19:31:32 +0930

    More movement, basic gravity

--- engine.py
+++ engine.py
@@ -7,18 +7,34 @@ class World:
         self.height = height
 
 class Player:
-    def __init__(self,x,y,image):
+    def __init__(self,x,y,image,horSpeed=1,maxGravSpeed=3):
         self.x = x
         self.y = y
         self.sprite = pyglet.sprite.Sprite(image,x=x,y=y)
         self.key_handler = key.KeyStateHandler()
+        self.horSpeed = horSpeed
+        self.maxGravSpeed = maxGravSpeed
+        self.gravity = 0
+
+    def gravitate(self):
+        if self.y > 64:
+            if self.gravity < self.maxGravSpeed:
+                self.gravity += 0.05
+        elif self.key_handler[key.SPACE] != True: self.gravity = 0
 
     def update(self):
         self.sprite.x = self.x
         self.sprite.y = self.y
+        self.y -= self.gravity
         if self.key_handler[key.A]:
-            self.x -= 1
-            print 'a'
+            self.x -= self.horSpeed
+        if self.key_handler[key.D]:
+            self.x += self.horSpeed
+        if self.key_handler[key.SPACE]:
+            self.gravity = -2
+        self.gravitate()
+
+
 
 
     def draw(self):
--- main.py
+++ main.py
@@ -5,12 +5,13 @@ playerImage = pyglet.image.load('resources/player.png')
 
 window = pyglet.window.Window(640,480)
 world = engine.World(640,480)
-player = engine.Player(10,10,playerImage)
+player = engine.Player(10,500,playerImage)
 
 window.push_handlers(player.key_handler)
 
 @window.event
 def on_draw():
+    window.clear()
     player.draw()
 
 def update(dt):
